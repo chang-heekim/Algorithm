560e406e8092391396b9c29eb5bbd3253fde89f8
chang-heekim <dkdlvhs9705@gmail.com>
2022-07-04 23:01:59 +0900

    update solution

---
+++ BaekJoon/Implementation/12904_A와B.py
@@ -0,0 +1,15 @@
+S = input()
+T = input()
+
+while True:
+    if T[-1] == "A":
+        T = T[:-1]
+    else:
+        T = T[:-1]
+        T = T[::-1]
+    if not T:
+        print(0)
+        break
+    if T == S:
+        print(1)
+        break
